ebef981a9d0a95ee74175a90ede8b01c08ccbff5
djshampz <gitau.marto@gmail.com>
2020-08-11 23:13:06 +0300

    database connection

--- src/logindatabase/FXMLDocumentController.java
+++ src/logindatabase/FXMLDocumentController.java
@@ -2,13 +2,20 @@
 package logindatabase;
 
 import com.jfoenix.controls.JFXButton;
+
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.animation.TranslateTransition;
@@ -20,13 +27,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import logindatabase.Helpers.DBConnector;
 import tray.animations.AnimationType;
 import tray.notification.NotificationType;
 import tray.notification.TrayNotification;
 
-
 /**
- *
  * @author JISOO
  */
 public class FXMLDocumentController implements Initializable {
@@ -81,8 +87,8 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane layer1;
     @FXML
     private Label l31;
-    
-    
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         s1.setVisible(false);
@@ -100,36 +106,36 @@ public class FXMLDocumentController implements Initializable {
         u2.setVisible(true);
         u3.setVisible(true);
         layer3.setVisible(false);
-    }    
+    }
 
     @FXML
     private void btn(MouseEvent event) {
         TranslateTransition slide = new TranslateTransition();
         slide.setDuration(Duration.seconds(0.7));
         slide.setNode(layer2);
-        
+
         slide.setToX(491);
         slide.play();
-        
-         ParallelTransition parallelTransition = new ParallelTransition();
+
+        ParallelTransition parallelTransition = new ParallelTransition();
         FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.7), layer1);
         fadeOut.setFromValue(1.0);
         fadeOut.setToValue(1.0);
         fadeOut.play();
-        
-        
+
+
         ParallelTransition parallelTransition2 = new ParallelTransition();
-        FadeTransition fadeOut2 = new FadeTransition(Duration.seconds(0.7), layer2 );
+        FadeTransition fadeOut2 = new FadeTransition(Duration.seconds(0.7), layer2);
         fadeOut2.setFromValue(0.5);
         fadeOut2.setToValue(1.0);
         fadeOut2.play();
-        
-        
+
+
         layer1.setTranslateX(-309);
         btnsignin.setVisible(true);
         b1.setVisible(true);
         b2.setVisible(true);
-        
+
         s1.setVisible(true);
         s2.setVisible(true);
         s3.setVisible(true);
@@ -149,10 +155,10 @@ public class FXMLDocumentController implements Initializable {
         u2.setVisible(false);
         u3.setVisible(false);
         layer3.setVisible(false);
-        
-        slide.setOnFinished((e->{
-        
-        
+
+        slide.setOnFinished((e -> {
+
+
         }));
     }
 
@@ -161,15 +167,15 @@ public class FXMLDocumentController implements Initializable {
         TranslateTransition slide = new TranslateTransition();
         slide.setDuration(Duration.seconds(0.7));
         slide.setNode(layer2);
-        
+
         slide.setToX(0);
         slide.play();
-        
+
         layer1.setTranslateX(0);
         btnsignin.setVisible(false);
         b1.setVisible(false);
         b2.setVisible(false);
-        
+
         s1.setVisible(false);
         s2.setVisible(false);
         s3.setVisible(false);
@@ -189,79 +195,103 @@ public class FXMLDocumentController implements Initializable {
         u2.setVisible(true);
         u3.setVisible(true);
         layer3.setVisible(false);
-        
-        slide.setOnFinished((e->{
-        
-        
+
+        slide.setOnFinished((e -> {
+
+
         }));
     }
 
     @FXML
-    private void btnsignup(MouseEvent event) {
+    private void btnsignup(MouseEvent event) throws SQLException {
+        Connection connection = DBConnector.getInstance().getConnection();
+        String username = u1.getText();
+        String email = u2.getText();
+        String password = u3.getText();
+
+        try {
+            Statement statement = connection.createStatement();
+
+            int status = statement.executeUpdate("insert into Users (users, email, password)" +
+                    "values ('" + username + "','" + email + "','" + password + "' )");
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        System.out.println("User registered");
     }
 
     @FXML
-    private void sign(MouseEvent event) {
-        
+    private void sign(MouseEvent event) throws SQLException {
+        String username, password;
+
+        username = n1.getText();
+        password = n2.getText();
+
+        Connection connection = DBConnector.getInstance().getConnection();
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from users where users = '" + username + "' " +
+                "+ password = '" + password + "'");
+
     }
 
     @FXML
-    private void click(ActionEvent event) throws IOException 
-    {
-        if("Alvin".equals(n1.getText())&&"OOP".equals(n2.getText())){
+    private void click(ActionEvent event) throws IOException {
+        if ("Alvin".equals(n1.getText()) && "OOP".equals(n2.getText())) {
             String tilte = "Sign In";
             String message = n1.getText();
             TrayNotification tray = new TrayNotification();
             AnimationType type = AnimationType.POPUP;
-        
+
             tray.setAnimationType(type);
             tray.setTitle(tilte);
             tray.setMessage(message);
             tray.setNotificationType(NotificationType.SUCCESS);
             tray.showAndDismiss(Duration.millis(3000));
-            
-            
+
+
             btnsignin.getScene().getWindow().hide();
             Stage dash = new stage();
             Parent root = FXMLLoader.load(getClass().getResource("dash.fxml"));
             Scene scene = new Scene(root);
             dash.setScene(scene);
             dash.show();
-            dash.setResizable(false);     
-            
-            
-            
+            dash.setResizable(false);
+
+
         }
-        if(!"Alvin".equals(n1.getText())){
+        if (!"Alvin".equals(n1.getText())) {
             String tilte = "Sign In";
-            String message = "Error Username "+"'"+n1.getText()+"'"+" Wrong";
+            String message = "Error Username " + "'" + n1.getText() + "'" + " Wrong";
             TrayNotification tray = new TrayNotification();
             AnimationType type = AnimationType.POPUP;
-        
+
             tray.setAnimationType(type);
             tray.setTitle(tilte);
             tray.setMessage(message);
             tray.setNotificationType(NotificationType.ERROR);
             tray.showAndDismiss(Duration.millis(3000));
         }
-        if (!"OOP".equals(n2.getText())){
+        if (!"OOP".equals(n2.getText())) {
             String tilte = "Sign In";
-            String message = "Error Password "+"'"+n2.getText()+"'"+" Wrong";
+            String message = "Error Password " + "'" + n2.getText() + "'" + " Wrong";
             TrayNotification tray = new TrayNotification();
             AnimationType type = AnimationType.POPUP;
-        
+
             tray.setAnimationType(type);
             tray.setTitle(tilte);
             tray.setMessage(message);
             tray.setNotificationType(NotificationType.ERROR);
             tray.showAndDismiss(Duration.millis(3000));
         }
-        if (!"Alvin".equals(n1.getText())&&!"OOP".equals(n2.getText())){
+        if (!"Alvin".equals(n1.getText()) && !"OOP".equals(n2.getText())) {
             String tilte = "Sign In";
-            String message = "Error Username "+"'"+n1.getText()+"'"+", and Password "+"'"+n2.getText()+"'"+" Wrong";
+            String message = "Error Username " + "'" + n1.getText() + "'" + ", and Password " + "'" + n2.getText() + "'" + " Wrong";
             TrayNotification tray = new TrayNotification();
             AnimationType type = AnimationType.POPUP;
-        
+
             tray.setAnimationType(type);
             tray.setTitle(tilte);
             tray.setMessage(message);
@@ -269,24 +299,24 @@ public class FXMLDocumentController implements Initializable {
             tray.showAndDismiss(Duration.millis(3000));
         }
     }
-    
-    
-     @FXML
+
+
+    @FXML
     private void btnsignin(MouseEvent event) {
-        
+
         TranslateTransition slide = new TranslateTransition();
         slide.setDuration(Duration.seconds(0.7));
         slide.setNode(layer2);
-        
+
         slide.setToX(491);
         slide.play();
-        
-         ParallelTransition parallelTransition = new ParallelTransition();
+
+        ParallelTransition parallelTransition = new ParallelTransition();
         FadeTransition fadeOut = new FadeTransition(Duration.seconds(0.7), layer3);
         fadeOut.setFromValue(1.0);
         fadeOut.setToValue(0.0);
         fadeOut.play();
-        
+
     }
 
     private static class stage extends Stage {
@@ -294,5 +324,5 @@ public class FXMLDocumentController implements Initializable {
         public stage() {
         }
     }
-    }
+}
 
